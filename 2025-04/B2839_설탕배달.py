299411073105419c09e7065d2143014e3903bc5d
stacy-sg <tmfrl3589@gmail.com>
2025-04-09 15:27:12 +0900

    2025-04-09

---
+++ 2025-04/B2839_설탕배달.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+cnt = 0
+while n >= 0:
+    if n % 5 == 0 :
+        cnt += (n // 5)
+        print(cnt)
+        break
+    n -= 3  
+    cnt += 1 
+else :
+    print(-1)
