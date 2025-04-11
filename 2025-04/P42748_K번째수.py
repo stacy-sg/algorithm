f3a989787a708664e880f6d2f0bd83e729e0f980
stacy-sg <tmfrl3589@gmail.com>
2025-04-11 18:19:45 +0900

    2025-04-11

---
+++ 2025-04/P42748_K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    tmp = []
+    for i in commands:
+        tmp = array[i[0]-1:i[1]]
+        tmp.sort()
+        answer.append(tmp[i[2]-1])
+    return answer
